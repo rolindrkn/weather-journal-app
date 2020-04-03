c76a0c0c2b88b835f41425e49a153a9d7eaf33f3
hulyak <57311842+hulya95@users.noreply.github.com>
2020-04-02 20:31:59 -0700

    add client-side code

--- website/app.js
+++ website/app.js
@@ -3,3 +3,26 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+
+const postData = async (url = '', data = {}) => {
+	console.log(data);
+	const response = await fetch(url, {
+		method: 'POST',
+		credentials: 'same-origin',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		// Body data type must match "Content-Type" header
+		body: JSON.stringify(data)
+	});
+
+	try {
+		const newData = await response.json();
+		console.log(newData);
+		return newData;
+	} catch (error) {
+		console.log('error', error);
+	}
+};
+
+postData('/add', { answer: 42 });
